8d5c814c56cb4598173fd88f764c9ea6f52e7203
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2018-08-16 17:10:37 +0200

    Skandapurāṇa Project

---
+++ sanskrit/sp/tf/otext.tf
@@ -0,0 +1,17 @@
+@config
+@convertedBy=Dirk Roorda
+@createdBy=Peter Bisschop et al.
+@description=volumes I-III of the critical edition of the Skandapurāṇa online
+@fmt:text-orig-full={dchar}{last}
+@fmt:text-orig-word={dword} 
+@fmt:text-trans-word={tword} 
+@name=Adhyāyas
+@provenance=https://www.universiteitleiden.nl/en/research/research-projects/humanities/the-skandapurāṇa-project
+@sectionFeatures=name,number,number
+@sectionTypes=text,verse,line
+@source1=http://hum2.leidenuniv.nl/pdf/skandapurana-project/SP_all_devanagari.zip
+@source2=http://hum2.leidenuniv.nl/pdf/skandapurana-project/SP_all_transliteration.zip
+@title=Skandapurāṇa Project
+@writtenBy=Text-Fabric
+@dateWritten=2018-08-16T13:55:36Z
+
---
+++ sanskrit/sp/tf/otype.tf
@@ -0,0 +1,18 @@
+@node
+@convertedBy=Dirk Roorda
+@createdBy=Peter Bisschop et al.
+@description=volumes I-III of the critical edition of the Skandapurāṇa online
+@name=Adhyāyas
+@provenance=https://www.universiteitleiden.nl/en/research/research-projects/humanities/the-skandapurāṇa-project
+@source1=http://hum2.leidenuniv.nl/pdf/skandapurana-project/SP_all_devanagari.zip
+@source2=http://hum2.leidenuniv.nl/pdf/skandapurana-project/SP_all_transliteration.zip
+@title=Skandapurāṇa Project
+@valueType=str
+@writtenBy=Text-Fabric
+@dateWritten=2018-08-16T13:55:35Z
+
+1-347770	char
+347771-392871	word
+392872-402883	line
+402884-407415	verse
+407416-407490	text
